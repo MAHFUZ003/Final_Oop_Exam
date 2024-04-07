aaa59786b6e4826246cb8cea9ce1a8ab521bf8bc
MAHFUZ003 <83081805+MAHFUZ003@users.noreply.github.com>
2024-04-07 12:23:33 +0600

    Update person.py

--- person.py
+++ person.py
@@ -9,6 +9,11 @@ class User(Person):
         super().__init__(email, password)
         self.balance = 0
         self.Transection_history = []
+    def trans_histry(self):
+        for s in self.Transection_history:
+            print(s)
+
+        
 
 
 class Admin(Person):
